562af50cc491904b086cccc9d29d375cca82fff7
Jose Buelvas <www.iho_jose@hotmail.com>
2018-01-29 16:42:33 -0500

    utilidades iniciales

---
+++ includes/path.js
@@ -0,0 +1,14 @@
+/**
+ * deeres-utilities was developed by Gammapeit SAS
+ *
+ * @name                deeres-utilities
+ * @version             1.0.1
+ * @date                29/01/2018
+ * @copyright           Gammapeit SAS (c) 2018
+ * @companyURL          https://www.gammapeit.com
+ * @platformURL         https://passqlub.com (http://bquest.co)
+ */
+module.exports = {
+	CDN: 'cdn/',
+	MODULES: 'module/'
+};
